ab0956df2d311c4b57465f17b1d79f34c6c3fd22
Con-Co-Industries <71508099+Con-Co-Industries@users.noreply.github.com>
2024-04-19 00:15:41 -0500

    Uploaded infoObtainer.java and fieldModifier.java

---
+++ reverse_engineering/fieldModifier.java
@@ -0,0 +1,46 @@
+// this file was written by Connor Ettinger (Team Axoltol) for challenge 2
+/* README - File Use Instructions:
+ * 1. Replace all four instances of "Countdown"
+ * (on lines 27, 30, and 41) with the name of
+ * the.class file (e.g., if the file is
+ * Challenge.class, replace Countdown
+ * with Challenge)
+ * 
+ * 2. go to line 34, and replace the first arguement
+ * with the number of the field you want to change
+ * (e.g, if you want to modify the 2nd field,
+ * change the first arguement to 1)
+ * 
+ * 3. replace the 2nd arguement with the value you
+ * want to change the field to
+ *     3.1 if the field's type isn't an int,
+ *     make sure to go to the function (line 41)
+ *     declaration and change the type as well
+ * 
+ * 4. Compile and Run!
+*/
+import java.lang.reflect.*;
+public class fieldModifer {
+    public static void main(String[] args) throws Exception {
+        // create the object we're dealing with
+        // TODO: replace "Countdown" with the name of the .class file
+        Countdown ex = new Countdown();
+
+        // generate list of fields in the class
+        Field [] fields = Countdown.class.getDeclaredFields();
+
+        // TODO: replace "field_num" with the number of the field you want to change
+        // TODO: replace "field_val" with the value you want to change the field to
+        fields = changeField(0, 10, ex, fields);
+        // for more info on what the arguements do, ask Connor
+
+        // finally, run the main class method with the modified field
+        ex.main(null);
+    }
+
+    private static Field[] changeField(int field_num, int field_val, Countdown cls, Field[] fields) throws Exception {
+        fields[field_num].setAccessible(true);
+        fields[field_num].set(cls, field_val);
+        return fields;
+    }
+}
---
+++ reverse_engineering/infoObtainer.java
@@ -0,0 +1,127 @@
+// file written by Connor Ettinger (Team Axoltol) for challenge 2
+/* README - File Use Instructions
+ * 1. Replace all three instances of "Test"
+ * (on lines 24 and 113) with the name of the
+ * .class file (e.g., if the file is Challenge.class,
+ * replace Test with Challenge)
+ * 
+ * 2.(in main) Comment out function calls
+ * for info you don't want (i.e., if you
+ * don't care about the constructor,
+ * comment out printConstructor())
+ * 
+ * 3. Compile and run!
+ */
+import java.lang.reflect.*;
+class infoObtainer {
+    public static void main(String[] args) throws Exception{
+        // README: I put everything into individual funtions, 
+        // so that you can just comment out fucntion calls
+        // for anything you don't want to see/do
+
+        // create the object to be inspected
+        // TODO: replace "Test" with the name of the .class file
+        Test ex = new Test();
+
+        // create a class file and store object in it (for finding class info)
+        Class c = ex.getClass();
+
+        // print all of the SuperClasses of the class
+        // the second arguement determines if we print all of c's super classes.
+        // if true, print all of c's ancestors, if false print only c's direct super
+        printSuperClass(c, true);
+
+        // print the constructor of the class
+        // TODO: figure out why we need this
+        printConstructor(c);
+
+        // print the methods of the class
+        // the second arguement is true if you want to print hidden methods
+        // and false if you just want public methods
+        printMethods(c, true);
+
+        // print all of the fields of the class
+        printFields(c);
+    }
+
+    private static void printSuperClass(Class c, boolean printAllSups) throws Exception{
+        System.out.println("\nGrabbing Super Class");
+        Class sup = c.getSuperclass();
+
+        // if the super class of c is the Object class,
+        // it doesn't have any super classes, so print that
+        if (sup.getName() == "java.lang.Object") {
+            // obviously, the Object Class is the super of all classes
+            // but since we don't care about it, it's easier to just say
+            // that it doesn't have a super class
+            System.out.println("This class has no super class");
+            System.out.println("\n---------------------------------------");
+        } else { // c has a super class
+            // print the super class of c
+            System.out.println("The super class of " + c.getName() + " is " + sup.getName());
+
+            // if we want to print all of ancestors of c
+            // (aka if printAllAncestors is true)
+            // make sure that sup isn't the Object class,
+            // because if it is then we're at the most superior class
+            while (sup.getName() != "java.lang.Object" && printAllSups){
+                // print the super class of sup
+                System.out.println("The super class of " + sup.getName() + " is " + sup.getSuperclass().getName());
+                // set sup equal to it's super class
+                sup = sup.getSuperclass();
+            }
+            System.out.println("\n---------------------------------------");
+        }
+    }
+
+    private static void printConstructor(Class c){
+        System.out.println("\nGrabbing Constructor");
+        // figure out if there is a constructor
+        try{
+            Constructor con = c.getConstructor();
+            System.out.println(con);
+        } catch (Exception e){
+            System.err.println("Constructor Not There");
+            System.err.println(e);
+        }
+        System.out.println("\n---------------------------------------");
+    }
+
+    private static void printMethods(Class c, boolean showHidden) {
+        System.out.println("\nGrabbing Methods");
+
+        if(showHidden){
+            // get all methods
+            Method[] allMethods = c.getDeclaredMethods();
+            // print them
+            for (Method m : allMethods){
+                System.out.println(m);
+            }
+        } else{
+            // get just the public methods
+            Method [] methods = c.getMethods();
+            // print them
+            for (Method m : methods){
+                System.out.println(m);
+            }
+        }
+        System.out.println("\n---------------------------------------");
+    }
+
+    private static void printFields(Class c) throws Exception{
+        System.out.println("\nGrabbing Fields");
+        Field[] fields = Test.class.getDeclaredFields();
+        int field_num = 0;
+        for (Field field : fields){
+            fields[field_num].setAccessible(true);
+            field.setAccessible(true);
+            System.out.println("field "+ field_num + ": " + field);
+            try {
+                System.out.println("field "+ field_num + " of class c: " + field.get(c) + "\n");
+            } catch (Exception e) {
+                System.out.println("cannot print value of this field, sorry\n");
+            }
+            field_num++;
+        }
+    }
+}
