d0bb901a15f645a860154fbf1031d6160f338d8e
Con-Co-Industries <71508099+Con-Co-Industries@users.noreply.github.com>
2024-04-26 11:13:42 -0500

    Updated fieldModifier.java
    Updated fieldModifier.java to include support for invoking methods

--- reverse_engineering/fieldModifier.java
+++ reverse_engineering/fieldModifier.java
@@ -1,12 +1,12 @@
 // this file was written by Connor Ettinger (Team Axoltol) for challenge 2
 /* README - File Use Instructions:
  * 1. Replace all four instances of "Example"
- * (on lines 27, 30, and 41) with the name of
+ * (on lines 27, 31, and 44) with the name of
  * the.class file (e.g., if the file is
  * Challenge.class, replace Example
  * with Challenge)
  * 
- * 2. go to line 34, and replace the first arguement
+ * 2. go to line 35, and replace the first arguement
  * with the number of the field you want to change
  * (e.g, if you want to modify the 2nd field,
  * change the first arguement to 1)
@@ -20,22 +20,25 @@
  * 4. Compile and Run!
 */
 import java.lang.reflect.*;
-public class fieldModifer {
+public class fieldModifier {
     public static void main(String[] args) throws Exception {
         // create the object we're dealing with
         // TODO: replace "Example" with the name of the .class file
         Example ex = new Example();
+        Class c = ex.getClass();
 
         // generate list of fields in the class
         Field [] fields = Example.class.getDeclaredFields();
 
         // TODO: replace "field_num" with the number of the field you want to change
         // TODO: replace "field_val" with the value you want to change the field to
-        fields = changeField(0, 10, ex, fields);
+        fields = changeField(0, 1000, ex, fields);
         // for more info on what the arguements do, ask Connor
 
-        // finally, run the main class method with the modified field
-        ex.main(null);
+        // finally, run the class method with the modified field
+        Method m = c.getDeclaredMethod("Insert Name Here");
+        m.setAccessible(true);
+        m.invoke(ex);
     }
 
     private static Field[] changeField(int field_num, int field_val, Example cls, Field[] fields) throws Exception {
